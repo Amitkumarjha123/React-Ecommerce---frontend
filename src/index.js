a928b74b70db0534b38b9856a82031b81c92445b
Amit Kumar Jha <43894506+Amitkumarjha123@users.noreply.github.com>
2023-01-13 12:58:40 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {HashRouter} from "react-router-dom";
+import {BrowserRouter} from "react-router-dom";
 import App from './App';
 import "antd/dist/antd.css";
 import {createStore} from "redux";
@@ -12,9 +12,9 @@ const store = createStore(rootReducer,composeWithDevTools());
 
 ReactDOM.render(
   <Provider store={store}>
-  <HashRouter>
+  <BrowserRouter>
   <App />
-  </HashRouter>
+  </BrowserRouter>
   </Provider>,
   
   document.getElementById('root')
